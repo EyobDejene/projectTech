bfe38dbad86dc9dd70c8647ad633bd5cfc7d0528
eyobdejene <eyob_westerink@hotmail.com>
2019-05-10 22:58:09 +0200

    edited readme file

---
+++ routes/overview.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var overview = express.Router();
+
+/* GET users listing. */
+overview.get('/', function(req, res, next) {
+    res.render('overview', { title: 'Overview' });
+});
+
+overview.get('/:id', function(req, res, next) {
+    res.render('overview', { title: req.params.id });
+});
+
+module.exports = overview;
